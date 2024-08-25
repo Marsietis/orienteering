f2a0d0b2ae6eb5f1e315007b43d068611198a8ca
Marsietis <c3xrk8o3@addy.io>
2024-08-25 15:42:27 +0300

    Added countdown timer

--- resources/js/Components/CountdownTimer.vue
+++ resources/js/Components/CountdownTimer.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <p>{{ hours }}h {{ minutes }}m {{ seconds }}s</p>
+        <p v-if="!eventEnded">{{ hours }}h {{ minutes }}m {{ seconds }}s</p>
+        <p v-else class="ended-message">Varžybos baigėsi / Event ended</p>
     </div>
 </template>
 
@@ -8,31 +9,41 @@
 export default {
     props: {
         eventEndDateTime: {
-            type: String,
+            type: Object,
             required: true,
+            default: () => ({ end_date: new Date() }) // Provide a default object to avoid null issues
         },
     },
     data() {
         return {
-            targetTime: new Date(this.eventEndDateTime).getTime(),
+            targetTime: this.eventEndDateTime ? new Date(this.eventEndDateTime.end_date).getTime() : new Date().getTime(),
             currentTime: new Date().getTime(),
             intervalId: null,
             days: 0,
             hours: 0,
             minutes: 0,
             seconds: 0,
+            eventEnded: false,
         };
     },
     methods: {
         updateCountdown() {
+            if (!this.eventEndDateTime || !this.eventEndDateTime.end_date) {
+                // Handle case where eventEndDateTime or end_date is not properly provided
+                this.eventEnded = true; // Set to true to avoid infinite loop
+                return;
+            }
+
             this.currentTime = new Date().getTime();
             const timeLeft = this.targetTime - this.currentTime;
 
             if (timeLeft < 0) {
                 clearInterval(this.intervalId);
+                this.eventEnded = true;
                 return;
             }
 
+            this.eventEnded = false; // Ensure the event is not marked as ended when there's still time
             this.days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
             this.hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             this.minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
@@ -53,5 +64,14 @@ export default {
 p {
     font-size: 2em;
     text-align: center;
+    margin: 0;
+}
+
+.ended-message {
+    font-size: 2em;
+    color: red;
+    text-align: center;
+    margin: 0;
+    font-weight: bold;
 }
 </style>
