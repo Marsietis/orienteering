54439b5a74d28970f1d993f3c7df02aa0828c46d
Marsietis <c3xrk8o3@addy.io>
2024-08-28 21:41:57 +0300

    Allowed knf.vu.lt emails to register

--- app/Rules/KnfEmailRule.php
+++ app/Rules/KnfEmailRule.php
@@ -10,8 +10,8 @@ class KnfEmailRule implements ValidationRule
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         // Validate that the value is a @knf.stud.vu.lt email address
-        if (!str_contains($value, '@knf.stud.vu.lt')) {
-            $fail('The ' . $attribute . ' must be a @knf.stud.vu.lt email address.');
+        if (!str_contains($value, '@knf.stud.vu.lt') && !str_contains($value, '@knf.vu.lt')) {
+            $fail('The ' . $attribute . ' must be a @knf.vu.lt email address.');
         }
     }
 }
